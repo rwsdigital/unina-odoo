1c6a9f5c725a0b702f3f9806d0cd2caec297c877
Francesco <Cesc@MacBook-Pro-di-Francesco.local>
2020-12-17 16:11:09 +0100

    UPD

--- addons-custom/rwsdigital_sale/models/sale.py
+++ addons-custom/rwsdigital_sale/models/sale.py
@@ -7,6 +7,8 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     test_field = fields.Char(string='Campo Test', store=True)
+    prova_integer = fields.Integer('Prova Integer')
+    description_unina = fields.Text('Desc Unina')
     #value = fields.Integer('')
     #value2 = fields.Float(compute="_value_pc", store=True)
     #description = fields.Text()
